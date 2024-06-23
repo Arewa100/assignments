19ee9bf3d84be7251d8bb2edb33197a214c85ccb
Arewa100 <olasorexway@gmail.com>
2024-06-23 23:37:02 +0100

    my assignment

---
+++ NumberPatterns.java
@@ -0,0 +1,37 @@
+public class NumberPatterns {
+	public static void main(String[] args) {
+		System.out.println("Welcome to my number pattern program");
+
+		int vertical;
+		int horizontal;
+	for(vertical = 0; vertical <= 6; vertical++) {
+		for(horizontal = 1; horizontal <= vertical; horizontal++) {
+			System.out.print(horizontal);
+}
+
+
+		System.out.println("");  
+}
+		System.out.println("");
+
+	for(vertical = 6; vertical >= 0; vertical--) {
+		for(horizontal = 1; horizontal <= vertical; horizontal++) {
+			System.out.print(horizontal);
+}
+		System.out.println("");
+}
+		System.out.println("");
+
+	for(vertical = 6; vertical >= 1; vertical--) {
+		for(horizontal = 1; horizontal <= vertical; horizontal++) {
+			System.out.print(" ");
+}	for(horizontal = 6; horizontal >=vertical; horizontal--) {
+			System.out.print(horizontal);
+}
+
+
+		System.out.println(" ");
+}
+	for(vertical 
+}
+}
