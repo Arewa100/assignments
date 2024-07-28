5b0e2ce14cd8b59aaa591edec2b86fbe1547afcb
Arewa100 <olasorexway@gmail.com>
2024-07-28 17:50:12 +0100

    functions update

---
+++ ListFunctions/java/ArrayFunctions.java
@@ -0,0 +1,15 @@
+public class ArrayFunctions {
+	
+	public int largest(int[] numbers) {
+		int largestNumber = 0;
+		for(int counter = 0; counter < numbers.length; counter++) {
+			if(numbers[counter] > largestNumber) {
+				largestNumber = numbers[counter];
+			}
+		}
+		return largestNumber;
+}
+	public int[] reverse(int[] numbers) {
+		int[] reveredArray = new
+}
+}
---
+++ ListFunctions/java/ArrayFunctionsTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayFunctionsTest {
+	
+	@Test
+	public void testingIfFunctionLargestCanReturnLargestElementInTheArray() {
+		ArrayFunctions feedback = new ArrayFunctions();
+		int[] value = {2, 4, 5, 15, 7, 8};
+		int result = feedback.largest(value);
+		
+		assertEquals(15, result);
+}
+	@Test
+	public void testingIfArrayFunctionReverseCanReverseAnArray() {
+		ArrayFunctions feedback = new Arrayfunctions();
+
+		int[] newArray = {8, 7, 6, 5, 4, 3};
+		int[] value = {3, 4, 5, 6, 7, 8};
+		int [] result = feedback.reverse(value);
+		
+		assertEquals(newArray, result);
+}
+}
