994392d206ca57c4fb45a26e3781dd1981c60a48
Arewa100 <olasorexway@gmail.com>
2024-08-03 13:08:12 +0100

    protest

---
+++ ProtestApp/python/protest.py
@@ -0,0 +1,8 @@
+from protest_demands import protester_feedback
+
+name = input("Enter your name:  ");
+print(f"welcome {name}")
+
+demand = protester_feedback()
+
+print(f"{demand}")
---
+++ ProtestApp/python/protest_demands.py
@@ -0,0 +1,7 @@
+import random
+def protester_feedback():
+	demand_list = ["end bad governance", "build good roads", "we don't want Tinubu", "give us electricity, we are tired", "end corruption", "we want good roads", "no protest, we want peace", "we want Tinubu", "things are expensive", "increase salaries", "end police brutality", "build our roads", "give us more jobs", "stop exportation", "increase minimum wage", "build our schools", "reduce the price of fuel", "divide Nigeria", "we want good governance", "tired of anti-government policies", "we condemn the cost of governance", "petrol pump and food prices is too high"]
+	
+	demand = random.choice(demand_list)
+	
+	return demand
