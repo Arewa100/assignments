adb4145c393a6506f52fad4a68d3acc67661a8a0
Arewa100 <olasorexway@gmail.com>
2024-08-06 14:03:46 +0100

    credit card validation

--- gateOne/creditCardValidation/python/validationfunction.py
+++ gateOne/creditCardValidation/python/validationfunction.py
@@ -1,7 +1,7 @@
 def double_every_second_number(credit_card_number):  
 	total = 0
 	for count in range(0, len(credit_card_number), 2):
-		value = int(credit_card_number[count] )
+		value = int(credit_card_number[count])
 		value = (value * 2)
 
 		if value >= 10:
@@ -15,6 +15,15 @@ def double_every_second_number(credit_card_number):
 	return total
 
 
-result = double_every_second_number("4388576018402626")
 
-print(result)
+def sum_of_numbers_in_odd_position(credit_card_number):
+	total = 0
+	for count in range(0, len(credit_card_number)):
+		value = int(credit_card_number[count]);
+		if not count % 2 == 0:
+			total = total + value
+	return total
+	
+def card_type(credit_card_number):
+	first_card = int(credit_card_number[0])
+	second_card = int(credit_card_number[0])
--- gateOne/creditCardValidation/python/validationtests.py
+++ gateOne/creditCardValidation/python/validationtests.py
@@ -1,4 +1,7 @@
-from validationfunction import double_every_second_number
+import validationfunction 
 	
 def test_double_every_second_number():
-	assert double_every_second_number("4388576018402626") == 37
+	assert validationfunction.double_every_second_number("4388576018402626") == 37
+
+def test_sum_of_numbers_in_odd_position():
+	assert validationfunction.sum_of_numbers_in_odd_position("4388576018402626") == 38
