6931e5dd1b0afed421a8092ef5b004667328cde9
Arewa100 <olasorexway@gmail.com>
2024-08-06 01:08:46 +0100

    credit card validator

--- gateOne/creditCardValidation/java/CreditCardValidationTest.java
+++ gateOne/creditCardValidation/java/CreditCardValidationTest.java
@@ -14,5 +14,65 @@ public class CreditCardValidationTest {
 	assertEquals(37, result);
 }
 	@Test
-	public void testingIfFunctionCanAddAllDigitInTheOddPlaces
+	public void testingIfFunctionCanAddAllDigitInTheOddPlaces() {
+	
+	ValidationFunction feedback = new ValidationFunction();
+		
+	int result = feedback.sumNumbersInOddPositions("4388576018402626");
+	
+	assertEquals(38, result);
+}
+	@Test
+	public void testingToCheckTheTypeOfCreditCardIsAVisaCard() {
+	
+	ValidationFunction feedback = new ValidationFunction();
+		
+	String result = feedback.cardType("4388576018402626");
+	
+	assertEquals("VisaCard", result);
+	
+}
+	@Test
+	public void testingToCheckTheTypeOfCreditCardIsaMasterCard() {
+	
+	ValidationFunction feedback = new ValidationFunction();
+		
+	String result = feedback.cardType("5388576018402626");
+	
+	assertEquals("MasterCard", result);
+	
+}
+	@Test
+	public void testingToCheckTheTypeOfCreditCardIsaAmericanExpressCards() {
+	
+	ValidationFunction feedback = new ValidationFunction();
+		
+	String result = feedback.cardType("3788576018402626");
+	
+	assertEquals("American Express Cards", result);
+	
+}
+	@Test
+	public void testingToCheckTheTypeOfCreditCardIsaDiscoverCard() {
+	
+	ValidationFunction feedback = new ValidationFunction();
+		
+	String result = feedback.cardType("6788576018402626");
+	
+	assertEquals("Discover Cards", result);
+	
+}
+	@Test
+	public void testingToCheckTheTypeOfCreditCardIfItIsInValid() {
+	
+	ValidationFunction feedback = new ValidationFunction();
+		
+	String result = feedback.cardType("2788576018402626");
+	
+	assertEquals("Invalid Card", result);
+	
+}
+	@Test
+	public void testingToSeeIfTheCardNumberIsValid() {
+}
 }
--- gateOne/creditCardValidation/java/ValidationFunction.java
+++ gateOne/creditCardValidation/java/ValidationFunction.java
@@ -1,6 +1,6 @@
 public class ValidationFunction {
 	
-	public int doubleEverySecondNumber(String creditCardNumber) {                 //"4388576018402626"
+	public int doubleEverySecondNumber(String creditCardNumber) {                 
 		final int STRING_LENGTH = creditCardNumber.length();  		      
 		
 		int count = (STRING_LENGTH - 2);
@@ -28,10 +28,52 @@ public class ValidationFunction {
 			
 			count = count - 2;
 		}
-		
-		
-		
 	
 	return total; 
+}
+	public int sumNumbersInOddPositions(String creditCardNumber) {
+		final int STRING_LENGTH = creditCardNumber.length();
+		int count = 0;
+		int totalOfNumbersInOddPositions = 0;
+
+		while(count < STRING_LENGTH) {
+
+			char value = creditCardNumber.charAt(count);
+			String stringOfNumber = String.valueOf(value);
+			int number = Integer.parseInt(stringOfNumber);
+			if(count % 2 != 0) {
+				totalOfNumbersInOddPositions = totalOfNumbersInOddPositions + number;
+			}
+			count = count + 1;
+		}
+	return totalOfNumbersInOddPositions;
+}
+	public String cardType(String creditCardNumber) {			////"4388576018402626"
+		char firstType = creditCardNumber.charAt(0);
+		char secondType = creditCardNumber.charAt(0);
+		char thirdTypeFirstDigit = creditCardNumber.charAt(0);
+		char thirdTypeSecondDigit = creditCardNumber.charAt(1);
+		char fourthType = creditCardNumber.charAt(0);
+		
+		int firstCardDigit = Integer.parseInt(String.valueOf(firstType));
+		int secondCardDigit = Integer.parseInt(String.valueOf(secondType));
+		int thirdCardFirstNumber = Integer.parseInt(String.valueOf(thirdTypeFirstDigit));
+		int thirdCardSecondNumber = Integer.parseInt(String.valueOf(thirdTypeSecondDigit));
+		int fourthCardDigit = Integer.parseInt(String.valueOf(fourthType));
+
+		if(firstCardDigit == 4) {
+			return "VisaCard";
+
+		} else if(secondCardDigit == 5) {
+			return "MasterCard";
+
+		} else if(thirdCardFirstNumber == 3 && thirdCardSecondNumber == 7) {
+			return "American Express Cards";
+
+		} else if(fourthCardDigit == 6) {
+			return "Discover Cards";
+		} else {
+			return "Invalid Card";
+		}
 }
 }
