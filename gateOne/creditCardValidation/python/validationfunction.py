acc3cca6adb93114b57791c9d2724f132cb5f5db
Arewa100 <olasorexway@gmail.com>
2024-08-06 14:43:44 +0100

    credit card python

--- gateOne/creditCardValidation/python/validationfunction.py
+++ gateOne/creditCardValidation/python/validationfunction.py
@@ -25,5 +25,59 @@ def sum_of_numbers_in_odd_position(credit_card_number):
 	return total
 	
 def card_type(credit_card_number):
+
 	first_card = int(credit_card_number[0])
 	second_card = int(credit_card_number[0])
+	third_card_first_digit = int(credit_card_number[0])
+	third_card_second_digit = int(credit_card_number[1])
+	fourth_card_number = int(credit_card_number[0])
+	
+	if first_card == 4:
+		return "VisaCard"
+
+	elif second_card == 5:
+		return "MasterCard"
+
+	elif third_card_second_digit == 3 and third_card_second_digit == 7:
+		return "American Express Cards"
+	elif fourth_card_number == 6:
+		return "Discover Cards"
+	else:
+		return "Invalid Card Type"
+
+def card(credit_card_number):
+
+	type_of_card = card_type(credit_card_number)
+	first_value = double_every_second_number(credit_card_number)
+	second_value = sum_of_numbers_in_odd_position(credit_card_number)
+	
+	length = len(credit_card_number)
+	card_digit_length = str(length)
+	
+	sum_of_the_values = (first_value + second_value)
+	
+	if sum_of_the_values % 10 == 0:
+		message =  "Credit Card Is Valid"
+	else:
+		message =  "Credit Card Is invalid"
+	
+	card_data = {"card_type": type_of_card, "card_number": credit_card_number, "card_digit_length": card_digit_length, "card_status": message}
+	
+	return card_data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- gateOne/creditCardValidation/python/validationtests.py
+++ gateOne/creditCardValidation/python/validationtests.py
@@ -5,3 +5,21 @@ def test_double_every_second_number():
 
 def test_sum_of_numbers_in_odd_position():
 	assert validationfunction.sum_of_numbers_in_odd_position("4388576018402626") == 38
+
+def test_card_type():
+	assert validationfunction.card_type("4388576018402626") == "VisaCard"
+
+def test_card_type():
+	assert validationfunction.card_type("5388576018402626") == "MasterCard"
+
+def test_card_type():
+	assert validationfunction.card_type("3788576018402626") == "American Express Cards"
+
+def test_card_type():
+	assert validationfunction.card_type("6388576018402626") == "Discover Cards"
+
+def test_card_type():
+	assert validationfunction.card_type("1388576018402626") == "Invalid Card Type"
+
+def test_card():
+	assert validationfunction.card("4388576018402626") == {"card_type": "VisaCard", "card_number": "4388576018402626", "card_digit_length": "16", "card_status": "Credit Card Is invalid"}
