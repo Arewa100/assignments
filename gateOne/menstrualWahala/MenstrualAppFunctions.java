c9243211765cee8ed9fa8b5ee42c44ee38125dab
Arewa100 <olasorexway@gmail.com>
2024-08-07 06:49:34 +0100

    menstrual app

--- gateOne/menstrualWahala/MenstrualAppFunctions.java
+++ gateOne/menstrualWahala/MenstrualAppFunctions.java
@@ -47,6 +47,54 @@ public class MenstrualAppFunctions {
 		System.out.print(Arrays.toString(calenderDays));
 		return calenderDays;
 }
+	public int[] flowDates(int day, int periodOfFlow, int[] calender) { 
+
+		int[] theFlowDates = new int[periodOfFlow];
+		
+		int counter = (day - 1);  
+		
+		int indexOfFlowDateArray = 0;
+
+		while(counter <= calender.length) {
+			if(indexOfFlowDateArray == periodOfFlow) {
+				counter = calender.length;
+			} else {
+				int datesOfFlow = calender[counter];
+				theFlowDates[indexOfFlowDateArray] = datesOfFlow; 
+				indexOfFlowDateArray = indexOfFlowDateArray + 1;
+			}
+			counter = counter + 1;
+		}
+		System.out.print(Arrays.toString(theFlowDates));
+		return theFlowDates;
+		
+		
+		
+}
+	public int ovulationDate(int averageMenstrualCycle, int[] calender, int day, int periodOfFlow) {
+		final int TIME_TAKEN = 14;
+		final int TIME_TAKEN_BEFORE_OVULATION = (averageMenstrualCycle - TIME_TAKEN); 
+
+		int endOfPeriodFlow = day + (periodOfFlow - 1); 
+		int count = (endOfPeriodFlow - 1); 
+		int startCounter = (count + 1);
+		int indexOfOvulationDateArray = 0;
+		int ovulDate = 0;
+
+		while (startCounter <= calender.length) {
+			if(indexOfOvulationDateArray == TIME_TAKEN_BEFORE_OVULATION) {
+				startCounter = calender.length;
+			} else {
+				ovulDate = calender[startCounter];
+				indexOfOvulationDateArray = indexOfOvulationDateArray + 1;
+				//this is where i am, ovulation dates 
+			}
+			startCounter = startCounter + 1;
+		}
+		return ovulDate;
+		
+		
+}
 }
 
 
--- gateOne/menstrualWahala/MenstrualAppFunctionsTest.java
+++ gateOne/menstrualWahala/MenstrualAppFunctionsTest.java
@@ -7,26 +7,58 @@ public class MenstrualAppFunctionsTest {
 	@Test
 	public void testingIfDateFunctionCanReturnTheNumberOfDaysOfFirstMonthAndNumberOfDaysOfSecondMOnth() {
 	
-	MenstrualAppFunctions feedback = new MenstrualAppFunctions();
+		MenstrualAppFunctions feedback = new MenstrualAppFunctions();
 	
-	int[] numberOfDaysOfTwoMonth = {30, 31};
-	int[] result = feedback.date(6, 7, 2024);
+		int[] numberOfDaysOfTwoMonth = {30, 31};
+		int[] result = feedback.date(6, 7, 2024);
 	
-	assertArrayEquals(numberOfDaysOfTwoMonth, result);
+		assertArrayEquals(numberOfDaysOfTwoMonth, result);
 }
 	@Test 
 	public void testingIfCalenderFunctionCanReturnCalenderOfTwoMonth() {
 	
 	MenstrualAppFunctions feedback = new MenstrualAppFunctions();
 	
-	int[] numberOfDaysOfTwoMonth = {30, 31};
+		int[] numberOfDaysOfTwoMonth = {30, 31};
 
-	int[] calenderOfTwoMonth = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+		int[] calenderOfTwoMonth = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 
-	int[] result = feedback.calender(numberOfDaysOfTwoMonth);
+		int[] result = feedback.calender(numberOfDaysOfTwoMonth);
 	
-	assertArrayEquals(calenderOfTwoMonth, result);
+		assertArrayEquals(calenderOfTwoMonth, result);
 	
 }
-	
+	@Test
+	public void testingIflowDatesFunctionToSeeIfItCAnReturnFlowDates() {
+
+		MenstrualAppFunctions feedback = new MenstrualAppFunctions();
+
+		int[] calenderOfTwoMonth = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+
+		int[] flowDays = {7, 8, 9, 10, 11};
+		int[] result = feedback.flowDates(7, 5, calenderOfTwoMonth);
+		
+		assertArrayEquals(flowDays, result);
+		
+		
 }
+	@Test
+	public void testingIfOvulationDatesFunctionCanReturnOvulationDate() {
+
+		MenstrualAppFunctions feedback = new MenstrualAppFunctions();
+		
+		int[] calenderOfTwoMonth = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+
+		int result = feedback.ovulationDate(28, calenderOfTwoMonth, 7, 5);
+		
+		assertEquals(25, result);
+		
+}
+}
+
+
+
+
+
+
+
