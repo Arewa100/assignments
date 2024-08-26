35e9b75df399adcaea249c9af109f3c7bfb752a7
Arewa100 <olasorexway@gmail.com>
2024-08-27 00:44:56 +0100

    bank app

---
+++ GTbank/Account/test/account/AccountTest.java
@@ -0,0 +1,11 @@
+package account;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    public void testingToConfirmAccountExists() {
+        Account account = new Account();
+    }
+}
