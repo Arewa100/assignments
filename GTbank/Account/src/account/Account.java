f0f2a9aee6913cfbf50a467a1151f6e0f0dd4bed
Arewa100 <olasorexway@gmail.com>
2024-08-27 06:27:23 +0100

    bank account

--- GTbank/Account/src/account/Account.java
+++ GTbank/Account/src/account/Account.java
@@ -1,4 +1,36 @@
 package account;
 
 public class Account {
+
+    private double balance;
+    private String name;
+    private String pin;
+    private int accountNumber;
+
+    public Account(int accountNumber, String firstName, String lastName, String pin) {
+        name = firstName + " " + lastName;
+        this.pin = pin;
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(int accountNumber, int amount, String pin) {
+        if(accountAndPinIsValid(accountNumber, pin) && amount > 0) balance += amount;
+    }
+
+    public double getBalance(int accountNumber, String pin) {
+        if(accountAndPinIsValid(accountNumber, pin)) {
+            return balance;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public void withdraw(int accountNumber, int amount, String pin) {
+        boolean accountBalanceIsSufficient = (amount > 0 && amount < balance);
+        if(accountAndPinIsValid(accountNumber, pin) && accountBalanceIsSufficient) balance -= amount;
+    }
+    private boolean accountAndPinIsValid(int accountNumber, String pin) {
+        boolean accountIsValidAndPinIsCorrect = (this.pin.equals(pin) && this.accountNumber == accountNumber);
+        return accountIsValidAndPinIsCorrect ? true : false;
+    }
 }
--- GTbank/Account/test/account/AccountTest.java
+++ GTbank/Account/test/account/AccountTest.java
@@ -1,11 +1,93 @@
 package account;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountTest {
+    private Account account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new Account(1, "Folashade", "Idowu", "correctPin");
+    }
 
     @Test
     public void testingToConfirmAccountExists() {
-        Account account = new Account();
+        assertEquals(0, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToDepositTwoThousandAndGetTwoThousandAsAccountBalance() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToConfirmIfDepositCanBeDoneWhenTheWrongPinIsUsed() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.deposit(1, 1000, "wrongPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToDepositAThousandAndGetTwoThousandAsAccountBalance() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.deposit(1, 1000, "correctPin");
+        assertEquals(3000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToDepositMinusTwoThousandAndAccountBalanceRemainsTheSame() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.deposit(1, -2000, "correctPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToDepositTwoThousandAndWithdrawFiveThousandWhenBalanceIsTwoThousand() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+        account.withdraw(1, 5000, "correctPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToDepositTwoThousandAndWithdrawOneThousand() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.withdraw(1, 1000, "correctPin");
+        assertEquals(1000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToWithdrawANegativeAmount() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.withdraw(1, -1000, "correctPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToWithdrawZeroAmount() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.withdraw(1, 0, "correctPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToWithdrawWithTheWrongPin() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.withdraw(1, 1000, "wrongPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToDepositAndWithdrawWithTheWrongAccountNumber() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        account.deposit(2, 1000, "correctPin");
+        assertEquals(2000, account.getBalance(1, "correctPin"));
+    }
+    @Test
+    public void testToConfirmIfICanCheckBalanceWithWrongPinAndAccountNumber() {
+        account.deposit(1, 2_000, "correctPin");
+        assertEquals(2_000, account.getBalance(1, "correctPin"));
+        assertEquals(0, account.getBalance(1, "wrongPin"));
     }
 }
