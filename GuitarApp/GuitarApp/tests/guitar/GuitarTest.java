e692d9047f4914c0dbadd9eac45c9f53a7eec662
Arewa100 <olasorexway@gmail.com>
2024-09-04 23:49:42 +0100

    guitar app

---
+++ GuitarApp/GuitarApp/tests/guitar/GuitarTest.java
@@ -0,0 +1,13 @@
+package guitar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GuitarTest {
+    @Test
+    public void testThatAGuitarExists() {
+        Guitar guitar = new Guitar();
+    }
+
+}
