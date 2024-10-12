57caba6d461292cb72cb87fceb05e11f8d5f53fc
Arewa100 <olasorexway@gmail.com>
2024-10-12 19:13:26 +0100

    linked list

--- DataStructures/linkedList/linkedList/src/LinkedList/LinkedList.java
+++ DataStructures/linkedList/linkedList/src/LinkedList/LinkedList.java
@@ -3,11 +3,13 @@ package LinkedList;
 public class LinkedList {
     private Node head;
     private Node last;
+    private int size;
 
 
     public LinkedList() {
         head = null;
         last = null;
+        size = 0;
     }
     public void insert(int data) {
         if(headIsNotEmpty()) insertDataIfHeadIsNotEmpty(data);
@@ -22,9 +24,7 @@ public class LinkedList {
 
     private void insertDataIfHeadIsNotEmpty(int data) {
             Node newNode = new Node(data);
-            System.out.println(newNode.getData());
             last.insertCurrentNodeAtAddress(newNode);
-            System.out.println("this is the data " + last.getLinkedNode().getData());
             last = newNode;
     }
 
@@ -41,5 +41,36 @@ public class LinkedList {
 
     public Node getLastNode() {
         return head;
+    }
+
+    public int size() {
+        size = 0;
+        Node currentNode = head;
+        iterateThroughNodeAddresses(currentNode);
+       return size;
+    }
+
+    private void iterateThroughNodeAddresses(Node currentNode) {
+        while(currentNode.getLinkedNode() != null) {
+            size++;
+            currentNode = currentNode.getLinkedNode();
+        }
+    }
+    public void insertAt(int nodeIndex, int data) {
+        insertAtGivenIndex(nodeIndex, data);
+    }
+
+    private void insertAtGivenIndex(int nodeIndex, int data) {
+        Node newNode = new Node(data);
+        Node currentNode = head;
+        int indexCount = 0;
+        while(currentNode != null) {
+            if(indexCount == nodeIndex) {
+                newNode.insertCurrentNodeAtAddress(currentNode.getLinkedNode());
+                currentNode.insertCurrentNodeAtAddress(newNode);
+            }
+            indexCount = indexCount + 1;
+            currentNode = currentNode.getLinkedNode();
+        }
     }
 }
--- DataStructures/linkedList/linkedList/tests/LinkedList/LinkedListTest.java
+++ DataStructures/linkedList/linkedList/tests/LinkedList/LinkedListTest.java
@@ -14,7 +14,7 @@ public class LinkedListTest {
     }
 
     @Test
-    public void test_That_Add_2_Data_To_A_LinkedList() {
+    public void test_That_Insert_2_Data_To_A_LinkedList() {
         LinkedList linkedList = new LinkedList();
         linkedList.insert(20);
         assertEquals(20, linkedList.getLastData());
@@ -23,13 +23,40 @@ public class LinkedListTest {
     }
 
     @Test
-    public void test_To_Add_Two_Data_To_A_List_And_Confirm_If_The_First_Node_Contains_Address_Of_The_Second_Node() {
+    public void test_To_Insert_Two_Data_To_A_List_And_Confirm_If_The_First_Node_Contains_Address_Of_The_Second_Node() {
         LinkedList linkedList = new LinkedList();
         linkedList.insert(20);
         linkedList.insert(30);
         assertEquals(30, linkedList.getLastNode().getLinkedNode().getData());
     }
 
-    
+    @Test
+    public void test_To_Insert_Three_Data_To_A_List_And_Confirm_If_The_Each_Node_Contains_Address_Of_The_Next_Node() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insert(20);
+        linkedList.insert(30);
+        linkedList.insert(40);
+        assertEquals(40, linkedList.getLastNode().getLinkedNode().getLinkedNode().getData());
+    }
+
+    @Test
+    public void test_To_Check_The_Number_Of_Item_In_A_Linked_List() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insert(20);
+        linkedList.insert(30);
+        linkedList.insert(40);
+        assertEquals(2, linkedList.size());
+    }
+    @Test
+    public void test_To_insert_Data_To_List_At_A_Required_Address() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insert(20);
+        linkedList.insert(30);
+        linkedList.insert(40);
+        assertEquals(2, linkedList.size());
+        linkedList.insertAt(1, 71);
+        assertEquals(3, linkedList.size());
+
+    }
 
 }
