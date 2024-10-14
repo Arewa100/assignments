106d4e93a8f4a7e5d528dd44d9b4ebf213c7c26c
Arewa100 <olasorexway@gmail.com>
2024-10-14 03:22:58 +0100

    map

---
+++ DataStructures/map/map/src/map/Map.java
@@ -0,0 +1,153 @@
+package map;
+
+import java.util.Arrays;
+
+public class Map<T> {
+    private final String[] key;
+    private final T[] value;
+    private int capacity;
+    private int size;
+
+
+
+    public Map(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.key = new String[capacity];
+        this.value = (T[]) new Object[capacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void insert(String key, T value) {
+        insertWhenMapDoesNotContainKey(key, value);
+    }
+
+    private void insertWhenMapDoesNotContainKey(String key, T value) {
+        if(mapDoesNotContainKey(key)) {
+            int index = this.key.length - capacity;
+            this.key[index] = key;
+            this.value[index] = value;
+            capacity = capacity - 1;
+            size++;
+        }
+    }
+
+    private boolean mapDoesNotContainKey(String key) {
+        return !contains(key);
+    }
+
+    public T get(String key) {
+        int indexOfTheKey = getKeyIndex(key);
+        return this.value[indexOfTheKey];
+    }
+
+    private int getKeyIndex(String key) {
+        for (int index = 0; index < this.key.length; index++) {
+            if (this.key[index] == key) {
+                return index;
+            }
+        }
+        return 0;
+    }
+    public boolean contains(String key) {
+        if(keyIsInMap(key)) return true;
+        return false;
+    }
+
+    private boolean keyIsInMap(String key) {
+        for (int index = 0; index < this.key.length; index++) {
+            if (this.key[index] == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void update(String key, T value) {
+        int indexOfTheKey = getKeyIndex(key);
+        this.value[indexOfTheKey] = value;
+    }
+
+    public void delete(String key) {
+        deleteTheByUsingKey(key);
+    }
+
+    private void deleteTheByUsingKey(String key) {
+        int indexOfTheKey = getKeyIndex(key);
+        iterateThroughKeyArray(indexOfTheKey);
+        iterateThroughValueError(indexOfTheKey);
+        decreaseSize();
+        increaseCapacity();
+        System.out.println(Arrays.toString(this.key));
+        System.out.println(Arrays.toString(this.value));
+    }
+
+    private void decreaseSize() {
+        size = size - 1;
+    }
+
+    private void increaseCapacity() {
+        capacity = capacity + 1;
+    }
+
+    private void iterateThroughValueError(int indexOfTheKey) {
+        for (int index = 0; index < this.key.length; index++) {
+            if (indexOfTheKey == index) {
+                pushTheNextValueToPositionOfTheDeletedValue(index);
+                pushTheValueAfterThePreviousPushedValueToPositionOfPreviousPushedValue(index);
+                turnValueInPositionOfCurrentPushedValueToNull(index);
+            } else {
+                T value = this.value[index];
+                insertNextValue(index, value);
+            }
+        }
+    }
+    private void insertNextValue(int index, T value) {
+        this.value[index] = value;
+    }
+    private void pushTheNextValueToPositionOfTheDeletedValue(int index) {
+        this.value[index] = this.value[index + 1];
+    }
+    private void pushTheValueAfterThePreviousPushedValueToPositionOfPreviousPushedValue(int index) {
+        this.value[index + 1] = this.value[index + 2];
+    }
+
+    private void turnValueInPositionOfCurrentPushedValueToNull(int index) {
+        this.value[index + 2] = null;
+    }
+
+    private void insertNextKey(int index, String key) {
+        this.key[index] = key;
+    }
+    private void pushTheNextKeyToPositionOfTheDeletedKey(int index) {
+        this.key[index] = this.key[index + 1];
+    }
+    private void pushTheKeyAfterThePreviousPushedKeyToPositionOfPreviousPushedKey(int index) {
+        this.key[index + 1] = this.key[index + 2];
+    }
+
+    private void turnKeyInPositionOfCurrentPushedKeyToNull(int index) {
+        this.key[index + 2] = null;
+    }
+    private void iterateThroughKeyArray(int indexOfTheKey) {
+        for (int index = 0; index < this.key.length; index++) {
+            if (indexOfTheKey == index) {
+                pushTheNextKeyToPositionOfTheDeletedKey(index);
+                pushTheKeyAfterThePreviousPushedKeyToPositionOfPreviousPushedKey(index);
+                turnKeyInPositionOfCurrentPushedKeyToNull(index);
+
+            } else {
+                String value = this.key[index];
+                insertNextKey(index, value);
+            }
+        }
+    }
+
+    private void increaseCapacityWhenFull() {
+        if (capacity == 0) capacity = size * 2;
+    }
+
+}
--- DataStructures/map/map/tests/map/MapTest.java
+++ DataStructures/map/map/tests/map/MapTest.java
@@ -1,4 +1,108 @@
 package map;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MapTest {
+    @Test
+    public void test_That_Map_Is_Empty() {
+         Map<String> map = new Map<>(10);
+         assertEquals(0, map.size());
+    }
+    @Test
+    public void test_That_To_Insert_Key_Value_Pair_To_Empty_Map() {
+        Map<String> map = new Map<>(10);
+        assertEquals(0, map.size());
+         map.insert("firstName", "Arewaking");
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void test_To_Add_Two_Pairs_In_The_Map_And_Get_A_Particular_Value_By_Key() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "lagos");
+        assertEquals(2, map.size());
+        assertEquals("lagos", map.get("West"));
+    }
+
+    @Test
+    public void test_That_Key_Is_Present_In_The_Map() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "lagos");
+        assertTrue(map.contains("North"));
+        assertEquals(2, map.size());
+    }
+
+    @Test
+    public void test_That_Key_Is_Not_Present_In_The_Map() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "lagos");
+        assertFalse(map.contains("East"));
+        assertEquals(2, map.size());
+    }
+
+    @Test
+    public void test_To_Get_Value_Stored_In_A_Key() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "lagos");
+        map.insert("Name", "Arewaking");
+        assertEquals("Arewaking", map.get("Name"));
+    }
+
+    @Test
+    public void test_To_Insert_Pair_Of_Same_Key_To_Map_One_Pair_Should_Only_Be_Inserted() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("North", "Nasarrawa");
+        assertEquals(1, map.size());
+    }
+    @Test
+    public void test_To_UpDate_Value_Associated_With_A_Key() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "Lagos");
+        map.update("North", "Kastina");
+        assertEquals("Kastina", map.get("North"));
+        assertEquals(2, map.size());
+    }
+
+    @Test
+    public void test_To_Delete_Key_Value_Pair_Of_A_Map() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "lagos");
+        map.insert("king", "Arewaking");
+        map.insert("queen", "Jenifer");
+        map.insert("prince", "Caleb");
+        map.delete("queen");
+        assertEquals(4, map.size());
+    }
+
+    @Test
+    public void test_To_Delete_Key_Value_Pair_And_Get_The_Value_Of_A_Pair() {
+        Map<String> map = new Map<>(10);
+        map.insert("North", "Kogi");
+        map.insert("West", "lagos");
+        map.insert("king", "Arewaking");
+        map.delete("West");
+        assertEquals("Arewaking", map.get("king"));
+
+    }
+
+//    @Test
+//    public void test_That_Map_Expands_Size_When_It_Is_Full() {
+//        Map<String> map = new Map<>(10);
+//        for (int index = 1; index <= 10; index++) {
+//            map.insert("key"+index, "Kogi");
+//        }
+//        assertEquals(10, map.size());
+//        map.insert("newKey", "football");
+//        assertEquals("football", map.get("newKey"));
+//    }
+
 }
