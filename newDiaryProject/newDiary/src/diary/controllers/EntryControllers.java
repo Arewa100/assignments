0f19e760b9e0b1c880ca2e8a26ce198d3f376d61
Arewa100 <olasorexway@gmail.com>
2024-11-19 03:32:05 +0100

    diary app

--- newDiaryProject/newDiary/src/diary/controllers/EntryControllers.java
+++ newDiaryProject/newDiary/src/diary/controllers/EntryControllers.java
@@ -1,4 +1,21 @@
 package controllers;
 
+import data.models.Entry;
+import services.EntryServices;
+import services.ServicesForEntry;
+
 public class EntryControllers {
+    private EntryServices entryServices = new ServicesForEntry();
+
+    public String createEntry(String username, String title, String body, long entryId) {
+        return entryServices.createEntry(username, title, body, entryId);
+    }
+
+    public long getNumberOfCreatedEntries() {
+        return entryServices.countNumberOfEntries();
+    }
+
+    public Entry findEntryBhyId(String micheal, long entryId) {
+        return entryServices.findEntryById(micheal, entryId);
+    }
 }
--- newDiaryProject/newDiary/tests/controllers/EntryControllersTest.java
+++ newDiaryProject/newDiary/tests/controllers/EntryControllersTest.java
@@ -3,6 +3,7 @@ package controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import services.DiaryExceptions;
+import services.EntryException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +15,44 @@ public class EntryControllersTest {
         entryControllers = new EntryControllers();
     }
 
+    @Test
+    public void testThatControllerCanCreateEntry_NumberOfCreatedEntryIsOne() {
+        String feedback = entryControllers.createEntry("Sarah", "title", "body", 1);
+        assertEquals("entry created successfully", feedback);
+        assertEquals(1, entryControllers.getNumberOfCreatedEntries());
+    }
+    @Test
+    public void testToCreateAndExistingEntry_NumberOfCreatedEntryRemainsOne() {
+        String feedback = entryControllers.createEntry("Sarah", "title", "body", 1);
+        assertEquals("entry created successfully", feedback);
+        assertEquals(1, entryControllers.getNumberOfCreatedEntries());
+        entryControllers.createEntry("Sarah", "title2", "body2", 1);
+        assertEquals(1, entryControllers.getNumberOfCreatedEntries());
+    }
+
+    @Test
+    public void testToCreateTwoEntries_NumberOfCreatedEntryIsTwo() {
+        String feedback = entryControllers.createEntry("Sarah", "title", "body", 1);
+        assertEquals("entry created successfully", feedback);
+        String feedbackTwo = entryControllers.createEntry("Micheal", "title", "body", 1);
+        assertEquals("entry created successfully", feedbackTwo);
+        assertEquals(2, entryControllers.getNumberOfCreatedEntries());
+    }
+
+    @Test
+    public void testThatEntryControllerCanFindEmtry() {
+       entryControllers.createEntry("Sarah", "title", "body", 1);
+       entryControllers.createEntry("Micheal", "michealTitle", "body2", 1);
+       assertEquals("michealTitle", entryControllers.findEntryBhyId("Micheal", 1).getTitle());
+    }
+
+    @Test
+    public void testFindAnEntryThatDoesNotExist() {
+        entryControllers.createEntry("Sarah", "title", "body", 1);
+        entryControllers.createEntry("Micheal", "michealTitle", "body2", 1);
+        assertThrows(EntryException.class, () -> entryControllers.findEntryBhyId("ruth", 2));
+    }
+
+    @Test
+    public void t // this iswhere i am
  }
